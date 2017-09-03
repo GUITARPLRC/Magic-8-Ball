35b8b462bf585b19382ab22e7cb4bc65ffd46bc6
guitarplrc <guitarplrc@yahoo.com>
2017-09-03 15:58:10 -0500

    remove input on input submit

--- script.js
+++ script.js
@@ -1,88 +1,97 @@
 window.onload = function() {
-	
-	var shake = document.getElementById("shake");
-	var clear = document.getElementById("clear");
-	var question = document.getElementById("question");
-	var display = document.getElementById("display");
-	var pic = document.getElementById("picture");
-	var warn = document.getElementById("warn");
-	var randomNumber = 0;
-	var prevNumber = 0;
-	var answers = ["Signs Point To Yes", "My Sources Say No", "Ask Again Later", "Not A Good Idea",
-	 "As I See It, Yes", "Outlook Not So Good", "Very Doubtful", "It Is Certain", "Concentrate And Ask Again",
-	 "Better Not Tell You Now"];
-	var length = answers.length;
-	var timer;
-	
+	var shake = document.getElementById('shake');
+	var clear = document.getElementById('clear');
+	var question = document.getElementById('question');
+	var display = document.getElementById('display');
+	var pic = document.getElementById('picture');
+	var warn = document.getElementById('warn');
+	var randomNumber;
+	var answers = [
+		'Signs Point To Yes',
+		'My Sources Say No',
+		'Ask Again Later',
+		'Not A Good Idea',
+		'As I See It, Yes',
+		'Outlook Not So Good',
+		'Very Doubtful',
+		'It Is Certain',
+		'Concentrate And Ask Again',
+		'Better Not Tell You Now'
+	];
+	let inputCheck = false;
+
 	question.focus();
 
-	clear.addEventListener("click", function(){
-		
-		toggleButtons();
-		
-		if (question.value !== ""){
-			
-		toggleImage();
-		
+	clear.addEventListener('click', function() {
+		if (inputCheck) {
+			toggleButtons();
+			toggleImage();
 		}
-		
-		question.value = "";
-		question.focus();
-		display.textContent = "";
-		display.style.visibility = "hidden";
-		clearTimeout(timer);
-		
+
+		display.textContent = '';
+		display.style.visibility = 'hidden';
+
+		setTimeout(function() {
+			question.value = '';
+			question.style.visibility = 'visible';
+			question.focus();
+		}, 500);
 	});
 
-	shake.addEventListener("click", function(){		
-	
-		toggleButtons();
-		
-		if (question.value === "") {
-			
-			warn.style.visibility = "visible";
+	function checkQuestion(input) {
+		let firstThree = input.slice(0, 3);
+		switch (firstThree) {
+			case 'who':
+				inputCheck = true;
+				break;
+			case 'wha':
+				inputCheck = true;
+				break;
+			case 'whe':
+				inputCheck = true;
+				break;
+			case 'how':
+				inputCheck = true;
+				break;
+			default:
+				inputCheck = false;
+		}
+	}
+
+	shake.addEventListener('click', function() {
+		checkQuestion(question.value);
+
+		if (inputCheck) {
+			toggleButtons();
+			question.style.visibility = 'hidden';
+			warn.style.visibility = 'hidden';
+			randomNumber = Math.floor(Math.random() * answers.length);
+
+			toggleImage();
+			setTimeout(function() {
+				display.style.visibility = 'visible';
+				display.classList.toggle('fade');
+				display.textContent = answers[randomNumber];
+			}, 2300);
+		} else {
+			warn.style.visibility = 'visible';
 			clear.click();
 			return;
-			
-		} else {
-		
-			warn.style.visibility = "hidden";
-			randomNumber = Math.floor(Math.random() * length);
-			
-			if (randomNumber === prevNumber) {
-				
-				randomNumber = Math.floor(Math.random() * length);
-				
-			}
-		
-			prevNumber = randomNumber;
-			toggleImage();	
-			timer = setTimeout(function(){
-				
-				display.style.visibility = "visible";
-				display.classList.toggle("fade");
-				display.textContent = answers[randomNumber];
-			
-			}, 2300);
 		}
-		
 	});
-	
+
 	function toggleImage() {
-			
-		pic.classList.toggle("shake");
-		pic.classList.toggle("big");
-		pic.classList.toggle("small");
-		
-	}
-	
-	function toggleButtons() {
-		
-		shake.classList.toggle("hide");
-		shake.classList.toggle("show");
-		clear.classList.toggle("hide");
-		clear.classList.toggle("show");
-		
+		pic.classList.toggle('shake');
+		setTimeout(function() {
+			pic.classList.toggle('big');
+		}, 200);
+		pic.classList.toggle('small');
 	}
 
+	function toggleButtons() {
+		shake.classList.toggle('hide');
+		shake.classList.toggle('show');
+		clear.classList.toggle('hide');
+		clear.classList.toggle('show');
+	}
 };
